2ce8c257db0bfcd7bb3454c941c335a3f1cfab81
beulrakbi <iamjyh99@gmail.com>
2025-08-05 11:57:12 +0900

    프로젝트 생성

---
+++ src/main/java/project/jlog/JlogApplication.java
@@ -0,0 +1,13 @@
+package project.jlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JlogApplication.class, args);
+    }
+
+}

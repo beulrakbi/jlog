bd59a9767a40fcdb9f7bce73cae004b9b50bea25
beulrakbi <iamjyh99@gmail.com>
2025-08-05 12:17:36 +0900

    Board 엔티티 생성 #1

---
+++ src/main/java/project/jlog/board/entity/Board.java
@@ -0,0 +1,23 @@
+package project.jlog.board.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "board")
+public class Board {
+    @Id //pk -> 인조식별자 사용
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //인조식별자 자동 증가로 중복 제거
+    private Long id;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "create_date")
+    private LocalDateTime date;
+}
